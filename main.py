f84b8973895e502141d866d27f38e7ebd55a8ff9
Valery Tolstov <testautomail@mail.ru>
2015-12-27 22:37:58 +0300

    Add examples

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import grammarparser
 import lrparser
-import io
+import sys
 
-str_grammar = ('S : A "b" | B "a" | S S;\n'
-               'A : "b";\n'
-               'B : "c";\n')
-str_io = io.StringIO(str_grammar)
-grparser = grammarparser.GrammarParser(str_io)
-rules = grparser.parse()
-grammar = lrparser.Grammar(rules)
-parser = lrparser.Parser(grammar)
-result = parser.parse("bbca")
-if not parser.error:
-    lrparser.print_res(result)
+if __name__ == "__main__":
+    fgrammar = open(sys.argv[1], 'r')
+    finput = open(sys.argv[2], 'r')
+
+    grparser = grammarparser.GrammarParser(fgrammar)
+    rules = grparser.parse()
+
+    grammar = lrparser.Grammar(rules)
+
+    parser = lrparser.Parser(grammar)
+    result = parser.parse(''.join(finput.readlines()))
+    if not parser.error:
+        lrparser.print_res(result)
